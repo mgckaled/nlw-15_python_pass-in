4f2aa300bf0739bdf13e19a7dea2d80258192a60
mgckaled <marcelgckaled@gmail.com>
2024-04-28 18:36:01 -0300

    feat: Implement declarative base and event entity

---
+++ src/models/entities/events.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+
+from src.models.settings.base import Base
+
+
+class Events(Base):
+    __tablename__ = "events"
+
+    id = Column(String, primary_key=True)
+    title = Column(String, nullable=False)
+    details = Column(String)
+    slug = Column(String, nullable=False)
+    maximum_attendees = Column(Integer)
