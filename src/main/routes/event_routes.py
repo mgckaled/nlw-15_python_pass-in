a7123d4d04f4e02d9040b82fed4487924cbbec4d
mgckaled <marcelgckaled@gmail.com>
2024-04-29 04:17:13 -0300

    feat: Implement event handler

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 
+from src.data.event_handler import EventHandler
 from src.http_types.http_request import HttpRequest
 
 event_route_bp = Blueprint("event_route", __name__)
@@ -8,6 +9,7 @@ event_route_bp = Blueprint("event_route", __name__)
 @event_route_bp.route("/events", methods=["POST"])
 def create_event():
     http_request = HttpRequest(body=request.json)
+    event_handler = EventHandler()
 
-    print(http_request)
-    return jsonify({"ola": "mundo"}), 200
+    http_response = event_handler.register(http_request)
+    return jsonify(http_response.body), http_response.status_code
